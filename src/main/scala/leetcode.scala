63191d0582ddf92117d237a683257e58ba9f50a3
MedyG <guohx3@hotmail.com>
2019-07-14 21:55:54 +0800

    strStr

--- src/main/scala/leetcode.scala
+++ src/main/scala/leetcode.scala
@@ -9,27 +9,84 @@ object leetcode {
 
 object Solution {
   def main(args: Array[String]): Unit = {
-    val l1 = new ListNode(1)
-    l1.next = new ListNode(2)
-    l1.next.next = new ListNode(6)
-    val l2 = new ListNode(1)
-    l2.next = new ListNode(3)
-    l2.next.next = new ListNode(5)
-    val l3 = new ListNode(4)
-    l3.next = new ListNode(7)
-    l3.next.next = new ListNode(8)
-    var l = timer(mergeTwoLists(l1, l2))._1
-    val arr = ArrayBuffer[Int]()
-    while (l != null) {
-      arr += l._x
-      l = l.next
+    println(maxArea(Array[Int](1, 8, 6, 2, 5, 4, 8, 3, 7)))
+    println(timer(strStr("a", "")))
+  }
+
+  /**
+    * https://leetcode.com/problems/implement-strstr/
+    *
+    * @param haystack
+    * @param needle
+    * @return
+    */
+  def strStr(haystack: String, needle: String): Int = {
+    val l: Int = needle.length
+    var head = 0
+    while (head + l <= haystack.length) {
+      if (haystack.substring(head, head + l) == needle) return head
+      head += 1
     }
-    println(arr.mkString("->"))
-    var ll = timer(mergeKLists(Array(l1,l3,l2)))._1
-    while (ll != null) {
-      println(ll._x)
-      ll = ll.next
+    -1
+  }
+
+  /**
+    * https://leetcode.com/problems/container-with-most-water/
+    * Given n non-negative integers a1, a2, ..., an ,
+    * where each represents a point at coordinate (i, ai).
+    * n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0).
+    * Find two lines, which together with x-axis forms a container,
+    * such that the container contains the most water.
+    *
+    * @param height
+    * @return
+    */
+  def maxArea(height: Array[Int]): Int = {
+    var max = 0
+    //    for (i <- height.indices){
+    //      val curH: Int = height(i)
+    //      if (curH > 0) {
+    //        val maxI: Int = i - max / curH
+    //        for (j <- 0 until maxI) {
+    //          val c: Int = (i - j) * math.min(curH, height(j))
+    //          max = math.max(max, c)
+    //        }
+    //      }
+    //    }
+    var left = 0
+    var right: Int = height.length - 1
+    while (left < right) {
+      max = math.max(max, math.min(height(left), height(right)) * (right - left))
+      if (height(left) < height(right)) {
+        left += 1
+      } else {
+        right -= 1
+      }
+
     }
+    max
+  }
+
+  /**
+    * https://leetcode.com/problems/search-insert-position/
+    * Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+    *
+    * You may assume no duplicates in the array.
+    *
+    * @param nums
+    * @param target
+    * @return
+    */
+  def searchInsert(nums: Array[Int], target: Int): Int = {
+    var i = 0
+    nums.foreach(n => {
+      if (n < target) {
+        i += 1
+      } else {
+        return i
+      }
+    })
+    i
   }
 
   /**
@@ -46,7 +103,7 @@ object Solution {
     if (lists.length == 2) return mergeTwoLists(lists(0), lists(1))
     var l = lists.head
     lists.tail.foreach(list => {
-      l = mergeTwoLists(l,list)
+      l = mergeTwoLists(l, list)
     })
     l
   }
@@ -68,8 +125,8 @@ object Solution {
   def mergeTwoLists(l1: ListNode, l2: ListNode): ListNode = {
     if (l1 == null) return l2
     if (l2 == null) return l1
-    var curl1 = l1
-    var curl2 = l2
+    var curl1: ListNode = l1
+    var curl2: ListNode = l2
     val l = if (curl1._x <= curl2._x) {
       val begin = curl1._x
       curl1 = curl1.next
